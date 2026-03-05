19c9fa8843ee94316094a4ad3b9045a13e69fe8d
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-03-05 21:21:16 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:foodserverapis", reload=True)
+    uvicorn.run("main:foodserverapis", reload=True)   # ← changed from app.main
