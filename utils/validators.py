91a60d3c3b12cfb5f4cfda387e3eecebcdd3cf48
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-03-06 05:54:33 +0200

    Update validators.py

--- utils/validators.py
+++ utils/validators.py
@@ -1,5 +1,6 @@
-import re
+from fastapi import HTTPException
 
 def validate_phone(phone: str):
-    pattern = r"^[0-9]{10,13}$"
-    return re.match(pattern, phone)
+    if not phone.startswith("0") or len(phone) != 10:
+        raise HTTPException(status_code=400, detail="Phone number must be 10 digits starting with 0")
+    return phone
