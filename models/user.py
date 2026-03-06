fc8cc0a78e4243bfd9d4bde2ebdfcfb7d4db97f9
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-03-06 05:47:05 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,9 +1,10 @@
 from sqlalchemy import Column, Integer, String
-from ..database import Base
+from database import Base
 
 class User(Base):
     __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True)
-    password = Column(String)
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    full_name = Column(String)
+    phone = Column(String)
