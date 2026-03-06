a9baadc6f273598a9178ebe3cd4e07f934403916
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-03-06 05:44:07 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
-import os
+# config.py
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./kotabites.db")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
 
-PAYSTACK_SECRET = os.getenv("PAYSTACK_SECRET")
+PAYSTACK_SECRET_KEY = os.getenv("PAYSTACK_SECRET_KEY")
+PAYSTACK_PUBLIC_KEY = os.getenv("PAYSTACK_PUBLIC_KEY")
 
 CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
 CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
 CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
-
-FRONTEND_URL = os.getenv("FRONTEND_URL")
