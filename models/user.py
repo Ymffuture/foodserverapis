869599730d6bf30e8f0f8bde0948e5376ee4f100
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-03-07 06:55:06 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Column, Integer, String
-from database import Base
+from beanie import Document
+from pydantic import EmailStr
 
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    full_name = Column(String)
-    phone = Column(String)
+class User(Document):
+    email: EmailStr
+    hashed_password: str
+    full_name: str
+    phone: str
+
+    class Settings:
+        name = "users"
