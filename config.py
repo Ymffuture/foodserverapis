754736284637aa00b69c1df373deabd0b4f99093
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-03-12 14:41:09 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "mongodb://localhost:27017/kotabites")
+DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("JWT_SECRET", "changeme")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
