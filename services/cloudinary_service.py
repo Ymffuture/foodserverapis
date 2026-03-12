6176d16b586789a0844860237d059a32a06157de
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-03-12 15:10:33 +0200

    Update cloudinary_service.py

--- services/cloudinary_service.py
+++ services/cloudinary_service.py
@@ -1,5 +1,8 @@
+# services/cloudinary_service.py
 import cloudinary
 import cloudinary.uploader
+import asyncio
+from functools import partial
 from config import CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET
 
 cloudinary.config(
@@ -8,6 +11,16 @@ cloudinary.config(
     api_secret=CLOUDINARY_API_SECRET
 )
 
-def upload_image(file):
-    result = cloudinary.uploader.upload(file.file)
+async def upload_image(file) -> str:
+    loop = asyncio.get_event_loop()
+    contents = await file.read()
+    
+    upload_func = partial(
+        cloudinary.uploader.upload,
+        contents,
+        folder="kotabites",
+        resource_type="image"
+    )
+    
+    result = await loop.run_in_executor(None, upload_func)
     return result.get("secure_url")
